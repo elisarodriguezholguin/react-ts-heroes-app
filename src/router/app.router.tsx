87b21ffa0a4b9668c6b6578377dc19b11e0523f4
ElisaRodriguez95 <44856961+elisarodriguezholguin@users.noreply.github.com>
2026-09-18 00:17:39 -0500

    gifs visualizados

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,11 +1,15 @@
-import { createBrowserRouter } from 'react-router';
+import { lazy } from 'react';
+import { createBrowserRouter, Navigate } from 'react-router';
 
 import { AdminLayout } from '@/admin/layouts/AdminLayout';
 import { AdminPage } from '@/admin/pages/AdminPage';
 import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
 import { HeroPage } from '@/heroes/pages/hero/HeroPage';
 import { HomePage } from '@/heroes/pages/home/HomePage';
-import { SearchPage } from '@/heroes/pages/search/SearchPage';
+
+// import { SearchPage } from '@/heroes/pages/search/SearchPage';
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
 
 export const appRouter = createBrowserRouter([
   {
@@ -17,12 +21,17 @@ export const appRouter = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'heroes/1',
+        path: 'heroes/:idSlug',
         element: <HeroPage />,
       },
       {
         path: 'search',
         element: <SearchPage />,
+      },
+      {
+        path: '*',
+        // element: <h1>404</h1>,
+        element: <Navigate to="/" />,
       },
     ],
   },
